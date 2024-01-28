49f4299cd9deec8ea09c1938c3e17c64c1c25227
Aastha Mukherjee <53363503+AasthaMuk@users.noreply.github.com>
2024-01-28 09:45:35 +0530

    1 commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 icon = Image.open("./images/home.jpg")
 st.set_page_config(page_title="Mother's Kitchen",page_icon= icon, layout='wide')
-st.image("./images/ungifted_amateur_v5.png", use_column_width=True )
+st.image("./images/download.jpeg", use_column_width=True )
 st.snow()
 
 _, exp_col, _ = st.columns([1,3,1])
