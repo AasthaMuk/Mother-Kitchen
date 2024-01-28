95e2ba3510b669216b81213aa782de23cca7e565
Aastha Mukherjee <53363503+AasthaMuk@users.noreply.github.com>
2024-01-28 13:25:37 +0530

    1 commit

--- app.py
+++ app.py
@@ -20,6 +20,23 @@ with exp_col:
                     """)
         
 
+col1,col2=st.columns([0.5,2])
+with col1:
+    option = st.selectbox(
+    'Select the Recipe',
+    ('Macher Zhol', '', ''))
+with col2:
+    if option=="Macher Zhol":
+        st.markdown("<h1 style='text-align: center; color: black;'>Macher Zhol in my Mom's Style</h1>", unsafe_allow_html=True)
+        st.markdown("Ingredients")
+        st.markdown("1. ginger and green chilli paste")
+        st.markdown("2. half onion ( slight pasted)")
+        st.markdown("3. half onion (sliced)")
+        st.markdown("3. potatoes (sliced)")
+        st.markdown("4. tomato (one sliced into 4 pieces)")
+        st.markdown("5. mustard oil")  
+        st.markdown("6. ")
+
     
        
 
